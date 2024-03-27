1cfa3f6876c3e51412c89a392d03518044a262c2
Dang Nguyen Tien Hau <tienhau.it@gmail.com>
2024-03-27 22:39:48 +0700

    Di chuyển thông tin kết nối DB sang file config/database.js & tái sử dụng connection

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3010;
 const hostname = process.env.HOST_NAME;
 const webRoutes = require('./routes/web');
 const configViewEngine = require('./config/viewEngine');
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 // config view engine & static files:
 configViewEngine(app);
@@ -13,24 +13,13 @@ configViewEngine(app);
 // init routers:
 app.use('/', webRoutes);
 
-// test connect:
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit',
-});
-
 // test query:
 connection.query (
     'SELECT * FROM Users',
     function (err, results, fields) {
         console.log("==> Results: ", results);
-        // console.log("==> Fields: ", fields);
     }
 );
-
 
 app.listen(port, hostname, (req, res) => {
     console.log(`App's running at port: ${port}`)
